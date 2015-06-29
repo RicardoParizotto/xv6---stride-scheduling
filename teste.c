6927cd8e0ca027948ef06450a1c449270e0f6bf9
Rafael <rafaelhr.ribeiro@gmail.com>
2015-06-29 14:37:04 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,26 +1,53 @@
+/*
+	Passar como parâmetro um número suficientemente grande para verificar a corretude do escalonador.
+	Redirecionar a saída para um arquivo para melhor visualização do escalonamento.
+	Exemplo de execução:
+		./teste 10000 > arquivo_teste
+*/
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
 
-int main()
+int stoi(char * str_num)
 {
-	int PID, i,k;
+        int k = 0;
+        char * ptr = str_num;
+        while(*ptr)
+                ptr++;
+        do   {
+                k *= 10;
+                k += str_num-'0';
+                ptr--;
+        } while(ptr != str_num);
+        return k;
+}
 
-	PID = fork(1000,1);
-
-	if(PID == 0)	{
-		for(i = 0; i < 1000; i++)
-			printf(1,"Filho %d\n",i);
-		exit();
-	} 
-	else if(PID > 0)	{
-		for(k = 0; k < 1000; k++)
-			printf(1,"Pai %d\n",k);
-		exit();
-	}
-	else
-		printf(1,"Erro\n");
-
-	return 0;
+int main(int argc, char **argv)
+{
+        int PID, i,k,l,j,T;
+ 
+        PID = fork(1000,1);
+       
+        T = stoi(argv[1]);
+ 
+        if(PID == 0)    {
+                for(i = 0; i < 100; i++)        {
+                        for(k = 0; k < T; k++);
+                        printf(1,"Filho %d - %d\n",i,k);
+                }
+                exit();
+        }
+        else if(PID > 0)        {
+                for(j = 0; j < 100; j++)        {
+                        for(l = 0; l < T; l++);
+                        printf(1,"Pai %d - %d\n",j,l);
+                }
+                exit();
+        }
+        else
+                printf(1,"Erro\n");
+ 
+        return 0;
 }
